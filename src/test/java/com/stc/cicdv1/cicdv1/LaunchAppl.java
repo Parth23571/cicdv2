88709c8ec3294915034a073958fe90e671b323f1
spselenim <pawal.satish@gmail.com>
2021-07-11 19:41:23 +0530

    FirstCommit

---
+++ src/test/java/com/stc/cicdv1/cicdv1/LaunchAppl.java
@@ -0,0 +1,29 @@
+package com.stc.cicdv1.cicdv1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LaunchAppl {
+	@Test
+	public void f() {
+		String strPath = "E:\\SeleniumTraining1\\TestingApp.html";
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		try {
+			driver.get(strPath);
+			System.out.println("Application Launched ......");
+			driver.findElement(By.id("PWD")).sendKeys("Hello");
+			driver.findElement(By.xpath("//a[text()='Forgot Password?']")).click();
+			String strTitle = driver.findElement(By.xpath("//h2")).getText();
+			System.out.println("Title is " + strTitle);
+			driver.quit();
+
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+}
